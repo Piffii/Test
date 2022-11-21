d5a873bbaf7f469150ff6e5ac9e97bc794185d29
Ученик303 <v.poletavkina@gmail.com>
2022-11-21 18:25:03 +0300

    four commit

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('olololololo')
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)
+        self.point = self.pushButton.clicked.connect(self.paintEvent())
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if not self.point:
+            return
+        painter = QPainter(self)
+        painter.setPen(QPen(Qt.black, 20.0))
+
+        painter.drawPoint(self.point)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
